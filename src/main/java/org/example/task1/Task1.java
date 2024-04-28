11bf6ce2c6bb1af05c8a64217300a69b7f2ecb8f
Egor Prozorov <yegorprozorov@yandex.ru>
2024-04-29 00:33:27 +0300

    first task done

---
+++ src/main/java/org/example/task1/Task1.java
@@ -0,0 +1,28 @@
+package org.example.task1;
+
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args){
+        if(args.length < 2){
+            System.out.println("Usage: java CircularArrayPath <n> <m>");
+        }
+        int n = Integer.parseInt(args[0]);
+        int m = Integer.parseInt(args[1]);
+
+        String answ = "";
+        int number = 1;
+        do {
+            answ += number;
+            number += m - 1;
+            if (number > n){
+                number -= n;
+            }
+        } while(number != 1);
+
+        System.out.println(answ);
+    }
+
+
+}
